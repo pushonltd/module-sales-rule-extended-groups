9169f913550df121a55393fbae91e2fd22c61644
Boris Rogachov <boris.rogachov@pushon.co.uk>
2026-09-06 22:59:14 +0300

    T-27174353: Support php8.5

--- .github/workflows/extdn-phpstan-pre-install.sh
+++ .github/workflows/extdn-phpstan-pre-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Installing the following modules to avoid phpstan errors on generated classes like:
 #       "Class ... not found"
-composer require --dev phpstan/extension-installer:^1.0 --no-update
-composer require --dev bitexpert/phpstan-magento:^0.32 --no-update
+composer require --dev -W \
+    "rector/rector ^2.0.0" \
+    "phpstan/phpstan ^2.0" \
+    "phpstan/extension-installer ^1.0" \
+    "bitexpert/phpstan-magento >=0.43.0"
